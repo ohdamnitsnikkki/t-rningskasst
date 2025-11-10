95a5bae9a87557aba890bd89bdcdd02bcb6a2674
ohdamnitsnikkki <mammastjernberg@gmail.com>
2025-11-10 11:31:54 +0000

    add def for summa_av_kast

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ import random
 # Funktionen som generar ett slumpmssigt tal
 def kasta_tarning():
     return random.randint(1, 6)
+
+# Funktonen som åberopar kasta_tarning två gånger samt summerar talen 
+def summa_av_kast():
+    tarning1 = kasta_tarning
+    tarning2 = kasta_tarning
+    total = tarning1 + tarning2
+    return total
